1261bd48ae6b029f5f5e6894ffd06bcb1b6c74fc
Tamas Gere <tgere@oneinc.com>
2023-07-04 02:55:01 +0200

    Finish push.sh

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker tag gtgt/mysql8-alpine:8.0.22-r0 gitlab.oneincsystems.com:5050/claimspay3/v3.2/mysql8-alpine:8.0.22-r0
+docker push gitlab.oneincsystems.com:5050/claimspay3/v3.2/mysql8-alpine:8.0.22-r0
